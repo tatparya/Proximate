59ef71b7087277f2e6dc00c6a234bdcb37324460
Tatparya Shankar <tshankar@purdue.edu>
2015-08-08 12:56:41 -0700

    Implemented Parse Push
    Implemented Parse Push Service to send notifications across devices

--- app/src/main/java/com/tatparya/proximate/ProximateApplication.java
+++ app/src/main/java/com/tatparya/proximate/ProximateApplication.java
@@ -3,7 +3,10 @@ package com.tatparya.proximate;
 import android.app.Application;
 import android.util.Log;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 /**
  * Created by Tatparya_2 on 7/25/2015.
@@ -30,6 +33,18 @@ public class ProximateApplication extends Application {
         //  ** Setting up Parse
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "kBvdtUIa0x3OaBzL1KbsN7rqzBEj0evV85UkUvNZ", "QonhJQPW6olGk5CU5DwHQ1bAXcklTk07yeaFF3MQ");
+
+        //  Subscribe to Parse Push Service
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
 
         mGoogleLocationService = new GoogleLocationService( getApplicationContext() );
         mGoogleLocationService.connectService();
